74796f069161786e9cae777915a0ca0bf53421ab
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2013-01-17 21:16:50 -0800

    Switch from ark to manual seems it seems not to have worked

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,24 @@
 gdal_version = node['gdal']['version']
 
-ark "gdal" do
-  url "http://download.osgeo.org/gdal/gdal-#{gdal_version}.tar.gz"
-  version gdal_version
-  action [:configure, :install_with_make]
-  creates "/usr/local/gdal-#{gdal_version}"
+tarball = "gdal-#{gdal_version}.tar"
+tarball_gz = "gdal-#{gdal_version}.tar.gz"
+remote_file "/tmp/#{tarball_gz}" do
+  source "http://download.osgeo.org/gdal/#{tarball_gz}"
+  mode "0644"
+  action :create_if_missing
+end
+
+bash "install_gdal_#{gdal_version}" do
+  untar_dir = "/usr/local/src"
+  user "root"
+  code <<-EOH
+    cd #{untar_dir} && \
+    tar xzvf /tmp/#{tarball_gz} && \
+    cd gdal-#{gdal_version} && \
+    ./configure && make && make install && \
+    ldconfig
+  EOH
+  command ""
+  creates untar_dir + "/gdal-#{gdal_version}"
+  action :run
 end
